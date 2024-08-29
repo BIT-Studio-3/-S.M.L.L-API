6fee67ccd8e3a2654d479b3b89baaa2efe2b527a
Logan Boyd (1000115752) <BOYDLP1@student.op.ac.nz>
2024-08-30 11:07:01 +1200

    set base file for routes

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -1,16 +1,33 @@
-import express from "express";
+import createRouter from "./base.js";
 
 import {
-    createUser,
-    getUsers,
-    getUser,
-    updateUser,
-    deleteUser,
+  getUser,
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
 } from "../../controllers/v1/user.js";
 
+import {
+  validatePostUser,
+  validatePutUser,
+} from "../../middlewares/validation.js";
 
-const router = express.Router();
+const userController = {
+  get: getUsers,
+  getById: getUser,
+  create: createUser,
+  update: updateUser,
+  delete: deleteUser,
+};
 
+const userRouter = createRouter(
+  userController,
+  validatePostUser,
+  validatePutUser
+);
+
+export default userRouter;
 
 
 /**
@@ -31,245 +48,240 @@ const router = express.Router();
  *           example: "Region Name"
  */
 
-/**
- * @swagger
- * /api/users:
- *   post:
- *     summary: Create a new User
- *     tags:
- *       - User
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
- *     responses:
- *       '201':
- *         description: User successfully created
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "User successfully created"
- *                 data:
- *                   type: array
- *                   items:
- *                     $ref: '#/components/schemas/User'
- *       '400':
- *         description: User with the same name already exists
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "User with the same name already exists"
- *       '500':
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "An unexpected error occurred"
- */
-router.post("/", createUser);
+// /**
+//  * @swagger
+//  * /api/users:
+//  *   post:
+//  *     summary: Create a new User
+//  *     tags:
+//  *       - User
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             $ref: '#/components/schemas/User'
+//  *     responses:
+//  *       '201':
+//  *         description: User successfully created
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "User successfully created"
+//  *                 data:
+//  *                   type: array
+//  *                   items:
+//  *                     $ref: '#/components/schemas/User'
+//  *       '400':
+//  *         description: User with the same name already exists
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "User with the same name already exists"
+//  *       '500':
+//  *         description: Internal server error
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "An unexpected error occurred"
+//  */
 
-/**
- * @swagger
- * /api/users:
- *   get:
- *     summary: Get all users
- *     tags:
- *       - User
- *     responses:
- *       '200':
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 data:
- *                   type: array
- *                   items:
- *                     $ref: '#/components/schemas/User'
- *       '404':
- *         description: No users found
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "No users found"
- *       '500':
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "An unexpected error occurred"
- */
-router.get("/", getUsers);
 
-/**
- * @swagger
- * /api/users/{id}:
- *   get:
- *     summary: Get an user by id
- *     tags:
- *       - User
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: The user id
- *     responses:
- *       '200':
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *       '404':
- *         description: No user found with the provided id
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "No user with the id: {id} found"
- *       '500':
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "An unexpected error occurred"
- */
-router.get("/:id", getUser);
+// /**
+//  * @swagger
+//  * /api/users:
+//  *   get:
+//  *     summary: Get all users
+//  *     tags:
+//  *       - User
+//  *     responses:
+//  *       '200':
+//  *         description: Success
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 data:
+//  *                   type: array
+//  *                   items:
+//  *                     $ref: '#/components/schemas/User'
+//  *       '404':
+//  *         description: No users found
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "No users found"
+//  *       '500':
+//  *         description: Internal server error
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "An unexpected error occurred"
+//  */
 
-/**
- * @swagger
- * /api/users/{id}:
- *   put:
- *     summary: Update an user by id
- *     tags:
- *       - User
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: The user id
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
- *     responses:
- *       '200':
- *         description: User successfully updated
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "User with the id: {id} successfully updated"
- *                 data:
- *                   $ref: '#/components/schemas/User'
- *       '404':
- *         description: No user found with the provided id
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "No user with the id: {id} found"
- *       '500':
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "An unexpected error occurred"
- */
-router.put("/:id", updateUser);
 
-/**
- * @swagger
- * /api/users/{id}:
- *   delete:
- *     summary: Delete an user by id
- *     tags:
- *       - User
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: The user id
- *     responses:
- *       '200':
- *         description: User successfully deleted
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "User with the id: {id} successfully deleted"
- *       '404':
- *         description: No user found with the provided id
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "No user with the id: {id} found"
- *       '500':
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "An unexpected error occurred"
- */
-router.delete("/:id", deleteUser);
+// /**
+//  * @swagger
+//  * /api/users/{id}:
+//  *   get:
+//  *     summary: Get an user by id
+//  *     tags:
+//  *       - User
+//  *     parameters:
+//  *       - in: path
+//  *         name: id
+//  *         required: true
+//  *         schema:
+//  *           type: string
+//  *         description: The user id
+//  *     responses:
+//  *       '200':
+//  *         description: Success
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               $ref: '#/components/schemas/User'
+//  *       '404':
+//  *         description: No user found with the provided id
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "No user with the id: {id} found"
+//  *       '500':
+//  *         description: Internal server error
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "An unexpected error occurred"
+//  */
 
-export default router;
+// /**
+//  * @swagger
+//  * /api/users/{id}:
+//  *   put:
+//  *     summary: Update an user by id
+//  *     tags:
+//  *       - User
+//  *     parameters:
+//  *       - in: path
+//  *         name: id
+//  *         required: true
+//  *         schema:
+//  *           type: string
+//  *         description: The user id
+//  *     requestBody:
+//  *       required: true
+//  *       content:
+//  *         application/json:
+//  *           schema:
+//  *             $ref: '#/components/schemas/User'
+//  *     responses:
+//  *       '200':
+//  *         description: User successfully updated
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "User with the id: {id} successfully updated"
+//  *                 data:
+//  *                   $ref: '#/components/schemas/User'
+//  *       '404':
+//  *         description: No user found with the provided id
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "No user with the id: {id} found"
+//  *       '500':
+//  *         description: Internal server error
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "An unexpected error occurred"
+//  */
+
+// /**
+//  * @swagger
+//  * /api/users/{id}:
+//  *   delete:
+//  *     summary: Delete an user by id
+//  *     tags:
+//  *       - User
+//  *     parameters:
+//  *       - in: path
+//  *         name: id
+//  *         required: true
+//  *         schema:
+//  *           type: string
+//  *         description: The user id
+//  *     responses:
+//  *       '200':
+//  *         description: User successfully deleted
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "User with the id: {id} successfully deleted"
+//  *       '404':
+//  *         description: No user found with the provided id
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "No user with the id: {id} found"
+//  *       '500':
+//  *         description: Internal server error
+//  *         content:
+//  *           application/json:
+//  *             schema:
+//  *               type: object
+//  *               properties:
+//  *                 message:
+//  *                   type: string
+//  *                   example: "An unexpected error occurred"
+//  */
