fe7b48b6e820373cb35519b80409a3ea58735818
dev-boyd <BOYDLP1@student.op.ac.nz>
2024-09-04 00:56:09 +1200

    update cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 // Configure CORS to allow requests from your frontend
 const corsOptions = {
   origin: 'https://s-m-l-l-webservice.onrender.com/',
-  origin: 'http://localhost:5173/', // Adjust this to your frontend URL
+  origin: 'http://localhost:3000/', // Adjust this to your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
