9e2b21b1f55fdc0a887b2564132b553563f6fc24
Mike <mcphmg2@student.op.ac.nz>
2024-09-16 17:15:18 +1200

    added controller for journal and .env file locally

---
+++ controllers/v1/journal.js
@@ -0,0 +1,39 @@
+/**
+ * @file Manages all operations related to users
+ * @author mike
+ * @description This function creates a new journal
+ * @param {object} req - The request object
+ * @param {object} res - The response object
+ * @returns {object} - The response object
+ */
+
+
+import {
+    createResource,
+    getResources,
+    getResource,
+    getEmailResource,
+    updateResource,
+    deleteResource,
+  } from "./base.js";
+
+  const createJournal = async (req, res) =>
+    createResource(req, res, "user");
+  
+  const getJournals = async (req, res) => getResources(req, res, "journal");
+  
+  const getJournal = async (req, res) => getResource(req, res, "journal");
+  
+  const updateJournal = async (req, res) =>
+    updateResource(req, res, "journal");
+  
+  const deleteJournal = async (req, res) =>
+    deleteResource(req, res, "journal");
+  
+  export {
+    createJournal,
+    getJournals,
+    getJournal,
+    updateJournal,
+    deleteJournal,
+  };
